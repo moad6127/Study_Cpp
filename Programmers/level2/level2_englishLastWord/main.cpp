2f929d99fcd5ae2c46163e4b6dc2c217c824a1bf
moad6127 <moad6127@gmail.com>
2023-06-12 22:19:50 +0900

    Feat : 프로그래머스 level2

---
+++ Programmers/level2/level2_englishLastWord/main.cpp
@@ -0,0 +1,38 @@
+#include <string>
+#include <vector>
+#include <unordered_set>
+using namespace std;
+
+vector<int> solution(int n, vector<string> words) {
+    vector<int> answer{0,0};
+    unordered_set<string> list;
+    char prevWord{words[0].back()};
+    list.insert(words[0]);
+
+    for (int i = 1; i < words.size(); i++)
+    {
+        auto itr = list.insert(words[i]);
+        if (itr.second)
+        {
+            if (words[i].front() == prevWord)
+            {
+                prevWord = words[i].back();
+            }
+            else
+            {
+                answer[0] = (i % n) + 1;
+                answer[1] = (i / n) + 1;
+                break;
+            }
+        }
+        else
+        {
+            answer[0] = (i % n) + 1;
+            answer[1] = (i / n) + 1;
+            break;
+        }
+
+    }
+
+    return answer;
+}
