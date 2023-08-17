9f660b71401250d59767aca59bde80cb69337fe2
moad6127 <moad6127@gmail.com>
2023-08-17 13:51:48 +0900

    Feat : 코딩연습
    완전탐색과 백트래킹2

--- CodingTestAlgorithm/Week3/Week3/BruteForce_backTracking/main.cpp
+++ CodingTestAlgorithm/Week3/Week3/BruteForce_backTracking/main.cpp
@@ -68,49 +68,56 @@ using namespace std;
 // 
 //
 
-int n;
-vector<int> v;
-
-bool check(int n)
-{
-	if (n <= 1)
-	{
-		return 0;
-	}
-	if (n == 2)
-	{
-		return 1;
-	}
-	if (n % 2 == 0)
-	{
-		return 0;
-	}
-	for (int i = 2; i * i <= n; i++)
-	{
-		if (n % i == 0)
-		{
-			return 0;
-		}
-	}
-	return 1;
-}
-
-int go(int idx, int sum)
-{
-	if (idx == n)
-	{
-		return check(sum);
-	}
-	return go(idx + 1, sum + v[idx]) + go(idx + 1, sum);
-}
-int main()
-{
-	cin >> n;
-	for (int i = 0; i < n; i++)
-	{
-		int temp;
-		cin >> temp;
-		v.push_back(temp);
-	}
-	cout << go(0,0) << '\n';
-}
+//int n;
+//vector<int> v;
+//
+//bool check(int n)
+//{
+//	if (n <= 1)
+//	{
+//		return 0;
+//	}
+//	if (n == 2)
+//	{
+//		return 1;
+//	}
+//	if (n % 2 == 0)
+//	{
+//		return 0;
+//	}
+//	for (int i = 2; i * i <= n; i++)
+//	{
+//		if (n % i == 0)
+//		{
+//			return 0;
+//		}
+//	}
+//	return 1;
+//}
+//
+//int go(int idx, int sum)
+//{
+//	if (idx == n)
+//	{
+//		return check(sum);
+//	}
+//	return go(idx + 1, sum + v[idx]) + go(idx + 1, sum);
+//}
+//int main()
+//{
+//	cin >> n;
+//	for (int i = 0; i < n; i++)
+//	{
+//		int temp;
+//		cin >> temp;
+//		v.push_back(temp);
+//	}
+//	cout << go(0,0) << '\n';
+//}
+//
+// 백트래킹
+// 
+// 완전탐색 방법에 가지치기를 사용하여 불필요한 탐색을 줄이는 방법이다.
+// 
+// 
+//
