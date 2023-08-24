c033c24b52430cf4ceead3258203b2eff03f8a7a
moad6127 <moad6127@gmail.com>
2023-08-24 17:13:48 +0900

    Feat : 코딩연습
    백준 16637
    괄호를 안하고 바로 계산하는 것과
    현재 계산하지않고 다음것을 괄호로 묶는 경우의 수2개를 생각해서 풀이한다.
    어렵다.

---
+++ CodingTestAlgorithm/Week3/Week3/Week3_F_16637/main.cpp
@@ -0,0 +1,60 @@
+#include <bits/stdc++.h>
+
+
+using namespace std;
+
+int N;
+string s;
+vector<int> nums;
+vector<char>oper_str;
+int result = -100000000;
+int oper(char a, int b, int c)
+{
+	if (a == '+')
+	{
+		return b + c;
+	}
+	if (a == '-')
+	{
+		return b - c;
+	}
+	if (a == '*')
+	{
+		return b * c;
+	}
+}
+void Func(int here, int _num)
+{
+	cout << here << " : " << _num << '\n';
+	if (here == nums.size() - 1)
+	{
+		result = max(result, _num);
+		return;
+	}
+	Func(here + 1, oper(oper_str[here], _num, nums[here + 1]));
+
+	if (here + 2 <= nums.size() - 1)
+	{
+		int temp = oper(oper_str[here + 1], nums[here + 1], nums[here + 2]);
+		Func(here + 2, oper(oper_str[here], _num, temp));
+	}
+}
+int main()
+{
+	cin >> N;
+	cin >> s;
+	for (int i = 0; i < s.size(); i++)
+	{
+		if (i % 2 == 0)
+		{
+			nums.push_back(s[i] - '0');
+		}
+		else
+		{
+			oper_str.push_back(s[i]);
+		}
+	}
+	Func(0, nums[0]);
+	cout<<result;
+
+}
