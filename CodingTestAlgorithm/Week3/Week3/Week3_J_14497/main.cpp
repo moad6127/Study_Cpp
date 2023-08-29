22b13829917bc6215752735d15a3bc08fcbf20a3
moad6127 <moad6127@gmail.com>
2023-08-29 21:33:51 +0900

    Feat : 코딩연습
    인강 풀이

--- CodingTestAlgorithm/Week3/Week3/Week3_J_14497/main.cpp
+++ CodingTestAlgorithm/Week3/Week3/Week3_J_14497/main.cpp
@@ -1,70 +1,62 @@
 #include <bits/stdc++.h>
 
 using namespace std;
-int N, M;
-vector<string> a;
-int visited[304][303];
-int jx1, jy1, chx2, chy2;
-const int dy[4] = { -1, 1, 0 ,0 };
-const int dx[4] = { 0,0, -1, 1 };
-int result;
-int check;
-
-int dfs(int y, int x)
-{
-	visited[y][x] = 1;
-	int check{};
-	if (a[y][x] =='#')
-	{
-		return 1;
-	}
-	if (a[y][x] == '1')
-	{
-		a[y][x] = '0';
-		return 0;
-	}
-	for (int i = 0; i < 4; i++)
-	{
-		int ny = y + dy[i];
-		int nx = x + dx[i];
-		if (ny < 0 || ny >= N || nx < 0 || nx >= M)
-		{
-			continue;
-		}
-		if (visited[ny][nx])
-		{
-			continue;
-		}
-		check += dfs(ny, nx);
-	}
-	return check;
-}
-
+int N, M, jx1, jy1, chx2, chy2;
+vector<string>a;
+int visited[304][304];
+const int dy[4] = { -1,1,0,0 };
+const int dx[4] = { 0 ,0 ,-1, 1 };
 int main()
 {
-	cin >> N >> M>>jx1>>jy1>>chx2>>chy2;
-	jx1 -= 1;
-	jy1 -= 1;
-	chx2 -= 1;
-	chy2 -= 1;
+	cin >> N >> M >> jy1 >> jx1 >> chy2 >> chx2;
+	jx1--;
+	jy1--;
+	chx2--;
+	chy2--;
 	for (int i = 0; i < N; i++)
 	{
 		string temp;
 		cin >> temp;
 		a.push_back(temp);
-
 	}
-	while (true)
+
+	queue<pair<int, int>>q;
+	q.push({ jy1,jx1 });
+	visited[jy1][jx1] = 1;
+	int check{};
+	while (a[chy2][chx2] != '0')
 	{
-		if (dfs(jx1, jy1) > 0)
+		check++;
+		queue<pair<int, int>>temp;
+		while (q.size())
 		{
-			result++;
-			break;
+			pair<int, int> now = q.front();
+			q.pop();
+			for (int i = 0; i < 4; i++)
+			{
+				int ny = now.first + dy[i];
+				int nx = now.second + dx[i];
+				if (ny < 0 || ny >= N || nx < 0 || nx >= M)
+				{
+					continue;
+				}
+				if (visited[ny][nx])
+				{
+					continue;
+				}
+				visited[ny][nx] = check;
+				if (a[ny][nx] != '0')
+				{
+					a[ny][nx] = '0';
+					temp.push({ ny,nx });
+				}
+				else
+				{
+					q.push({ ny,nx });
+				}
+			}
 		}
-		memset(visited, 0, sizeof(visited));
-		result++;
+		q = temp;
 	}
-
-	cout << result;
-	return 0;
+	cout << visited[chy2][chx2];
 }
