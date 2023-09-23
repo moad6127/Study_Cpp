c8a05f6b78ed33445424b66c7d5938e7c369aa03
moad6127 <moad6127@gmail.com>
2023-09-23 14:48:47 +0900

    Feat : 코딩연습
    비트마스킹 활용하기
    매개변수로 넘기기

--- CodingTestAlgorithm/Week4/Week4/Week4_BitMaskUseing/main.cpp
+++ CodingTestAlgorithm/Week4/Week4/Week4_BitMaskUseing/main.cpp
@@ -136,6 +136,30 @@ using namespace std;
 //
 //}
 // 
+
+const int n = 4;
+string a[4] = { "사과", "딸기", "포도","배" };
+
+void go(int num)
+{
+	string result = "";
+	for (int i = 0; i < 4; i++)
+	{
+		if (num & (1 << i))
+		{
+			result += a[i] + " ";
+		}
+	}
+	cout << result<<'\n';
+	return;
+}
+int main()
+{
+	for (int i = 1; i < n; i++)
+	{
+		go(1 | (1 << i));
+	}
+}
 //
 // 
 //
