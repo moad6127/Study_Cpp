8c6ceae9f9068e4f572b8df4a007e4a23e1176d4
moad6127 <moad6127@gmail.com>
2023-10-24 17:51:26 +0900

    Feat : 코딩연습
    백준 3015 스택활용해서 풀기

--- CodingTestAlgorithm/Week4/Week4/Week4_P_3015/main.cpp
+++ CodingTestAlgorithm/Week4/Week4/Week4_P_3015/main.cpp
@@ -2,37 +2,36 @@
 
 using namespace std;
 
-int N;
-int a[500004];
-int result;
+long long N, temp;
+long long result;
+stack<pair<long long, long long>> st;
 
 int main()
 {
 	cin >> N;
-	for (int i = 0; i < N; i++)
-	{
-		cin >> a[i];
-	}
 
 	for (int i = 0; i < N; i++)
 	{
-		int maxSize{0};
-		for (int j = i + 1; j < N; j++)
+		cin >> temp;
+		int cnt = 1;
+		while (st.size() && st.top().first <= temp)
 		{
-			if (a[i] < a[j])
+			result += st.top().second;
+			if (st.top().first == temp)
 			{
-				result++;
-				break;
+				cnt = st.top().second + 1;
 			}
 			else
 			{
-				if (maxSize <= a[j])
-				{
-					maxSize = a[j];
-					result++;
-				}
+				cnt = 1;
 			}
+			st.pop();
 		}
+		if (st.size())
+		{
+			result++;
+		}
+		st.push({ temp,cnt });
 	}
 	cout << result;
 }
