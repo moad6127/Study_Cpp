1955de55373c173247e9425041e8ba2e62ee4a45
moad6127 <moad6127@gmail.com>
2023-10-27 18:51:06 +0900

    Feat : 코딩연습
    백준 9935 스택을 활용해서 풀기

--- CodingTestAlgorithm/Week5/Week5_B_9935/main.cpp
+++ CodingTestAlgorithm/Week5/Week5_B_9935/main.cpp
@@ -1,30 +1,78 @@
-#include <bits/stdc++.h>
+//#include <bits/stdc++.h>
+//
+//using namespace std;
+//string A, B;
+//string result;
+//int main()
+//{
+//	ios_base::sync_with_stdio(false);
+//	cin.tie(NULL);
+//	cout.tie(NULL);
+//	cin >> A >> B;
+//
+//	for (auto e : A)
+//	{
+//		result += e;
+//		if (result.size() >= B.size() && result.substr(result.size() - B.size(), B.size()) == B)
+//		{
+//			result.erase(result.end() - B.size(), result.end());
+//		}
+//	}
+//
+//	if (result.size())
+//	{
+//		cout << result<<'\n';
+//	}
+//	else
+//	{
+//		cout << "FRULA" << '\n';
+//	}
+//}
+
+#include<bits/stdc++.h>
 
 using namespace std;
+
 string A, B;
+stack<char> st;
 string result;
 int main()
 {
-	ios_base::sync_with_stdio(false);
-	cin.tie(NULL);
-	cout.tie(NULL);
 	cin >> A >> B;
-
 	for (auto e : A)
 	{
-		result += e;
-		if (result.size() >= B.size() && result.substr(result.size() - B.size(), B.size()) == B)
+		st.push(e);
+		if (st.size() >= B.size() && st.top() == B.back())
 		{
-			result.erase(result.end() - B.size(), result.end());
+			string temp;
+			for (int i = 0; i < B.size(); i++)
+			{
+				temp += st.top();
+				st.pop();
+			}
+			reverse(temp.begin(), temp.end());
+			if (temp != B)
+			{
+				for (auto k : temp)
+				{
+					st.push(k);
+				}
+			}
 		}
 	}
-
+	int check = st.size();
+	for (int i = 0; i < check; i++)
+	{
+		result += st.top();
+		st.pop();
+	}
+	reverse(result.begin(), result.end());
 	if (result.size())
 	{
-		cout << result<<'\n';
+		cout << result;
 	}
 	else
 	{
-		cout << "FRULA" << '\n';
+		cout << "FRULA";
 	}
 }
