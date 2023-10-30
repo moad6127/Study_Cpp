01d5bafef5131f25e8f8f8d7677730f151c56514
moad6127 <moad6127@gmail.com>
2023-10-30 17:43:04 +0900

    Feat : 코딩연습
    코딩연습

---
+++ CodingTestAlgorithm/StepStudy/Step2/Step2/Step2_1_1330/main.cpp
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+int A, B;
+int main()
+{
+	cin >> A >> B;
+	if (A > B)
+	{
+		cout << ">";
+	}
+	else if (A < B)
+	{
+		cout << "<";
+	}
+	else
+	{
+		cout << "==";
+	}
+}
