9b1ad956c2ae83dcfe03adc71f7f3e2fd1e89735
moad6127 <moad6127@gmail.com>
2023-11-13 18:27:28 +0900

    Feat : 코딩연습
    백준 스텝풀이

---
+++ CodingTestAlgorithm/StepStudy/Step5/Step5_A1_27866/main.cpp
@@ -0,0 +1,11 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+string S;
+int i;
+int main()
+{
+	cin >> S >> i;
+	cout << S[i - 1];
+}
---
+++ CodingTestAlgorithm/StepStudy/Step5/Step5_B2_2743/main.cpp
@@ -0,0 +1,11 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+string  s;
+int main()
+{
+	cin >> s;
+	cout << s.size();
+
+}
