becf87a89520e64dab0070db1f636fcf068a020b
moad6127 <moad6127@gmail.com>
2024-01-30 18:39:11 +0900

    Feat : 코딩연습
     백준 16434 이분탐색으로 풀이

--- CodingTestAlgorithm/Week6/Week6_F_16434/main.cpp
+++ CodingTestAlgorithm/Week6/Week6_F_16434/main.cpp
@@ -2,34 +2,76 @@
 
 using namespace std;
 
-long long N, Hatk, t, a, h, damage,H,mx;
 
+//
+//long long N, Hatk, t, a, h, damage,H,mx;
+//
+//
+//int main()
+//{
+//	cin >> N >> Hatk;
+//
+//	for (int i = 0; i < N; i++)
+//	{
+//		cin >> t >> a >> h;
+//		if (t == 1)
+//		{
+//			damage = a * (ceil((double)h / Hatk) - 1);
+//			if (H < damage)
+//			{
+//				mx += damage - H;
+//				H = 0;
+//			}
+//			else
+//			{
+//				H -= damage;
+//			}
+//		}
+//		else
+//		{
+//			Hatk += a;
+//			H = min(H + h, mx);
+//		}
+//	}
+//	cout << mx + 1;
+//}
+
+
+long long n, attack, t[13005], a[13005], h[13005], lo, hi, result;
+
+bool check(long long mid)
+{
+	long long mxHP = mid;
+	long long init_attack = attack;
+	for (int i = 0; i < n; i++) {
+		if (t[i] == 2) {
+			mid = min(mxHP, mid + h[i]);
+			init_attack += a[i];
+		}
+		else {
+			long long cnt = h[i] / init_attack + (h[i] % init_attack ? 1 : 0);
+			mid -= (cnt - 1) * a[i];
+		}
+		if (mid <= 0) return false;
+	}
+	return true;
+}
 
 int main()
 {
-	cin >> N >> Hatk;
-
-	for (int i = 0; i < N; i++)
+	cin >> n >> attack;
+	for (int i = 0; i < n; i++)
 	{
-		cin >> t >> a >> h;
-		if (t == 1)
-		{
-			damage = a * (ceil((double)h / Hatk) - 1);
-			if (H < damage)
-			{
-				mx += damage - H;
-				H = 0;
-			}
-			else
-			{
-				H -= damage;
-			}
-		}
-		else
-		{
-			Hatk += a;
-			H = min(H + h, mx);
-		}
+		cin >> t[i] >> a[i] >> h[i];
 	}
-	cout << mx + 1;
+	lo = 1, hi = 1e18 + 4;
+	while (lo <= hi) {
+		long long mid = (lo + hi) / 2;
+		if (check(mid)) {
+			hi = mid - 1;
+			result = mid;
+		}
+		else lo = mid + 1;
+	}
+	cout << result;
 }
