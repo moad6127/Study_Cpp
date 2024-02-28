2cc03b4c9bc5fb3d111eb7e4ed44bf0c8cc310fe
moad6127 <moad6127@gmail.com>
2024-02-28 14:25:41 +0900

    Feat : 코딩연습
    백준 17070
    인터넷강의 풀이
    내가 푼것과 반대방향으로 풀었음

--- CodingTestAlgorithm/Week7/Week7_B_17070/main.cpp
+++ CodingTestAlgorithm/Week7/Week7_B_17070/main.cpp
@@ -1,57 +1,132 @@
+//#include <bits/stdc++.h>
+//
+//using namespace std;
+//
+//int N;
+//int a[20][20], check[3][20][20];
+//
+//int Func(int dir, int y, int x)
+//{
+//	if (y < 0 || y >= N || x < 0 || x >= N ||a[y][x] == 1)
+//	{
+//		return 0;
+//	}
+//	if (dir == 2)
+//	{
+//		if (a[y - 1][x] == 1 || a[y][x - 1] == 1)
+//		{
+//			return 0;
+//		}
+//	}
+//	if (y == N - 1 && x == N - 1)
+//	{
+//		return 1;
+//	}
+//	if (check[dir][y][x])
+//	{
+//		return check[dir][y][x];
+//	}
+//	if (dir == 0)
+//	{
+//		check[dir][y][x] = Func(0, y, x + 1) + Func(2, y + 1, x + 1);
+//	}
+//	else if (dir == 1)
+//	{
+//		check[dir][y][x] = Func(1, y + 1, x) + Func(2, y + 1, x + 1);
+//	}
+//	else
+//	{
+//		check[dir][y][x] = Func(0, y, x + 1) + Func(1, y + 1, x) + Func(2, y + 1, x + 1);
+//	}
+//	return check[dir][y][x];
+//}
+//
+//int main()
+//{
+//	cin >> N;
+//	for (int i = 0; i < N; i++)
+//	{
+//		for (int j = 0; j < N; j++)
+//		{
+//			cin >> a[i][j];
+//		}
+//	}
+//	cout << Func(0, 0, 1);
+//
+//	return 0;
+//}
+
 #include <bits/stdc++.h>
 
 using namespace std;
 
-int N;
-int a[20][20], check[3][20][20];
+int n, a[24][24], dp[24][24][3];
 
-int Func(int dir, int y, int x)
+bool check(int y, int x, int dir)
 {
-	if (y < 0 || y >= N || x < 0 || x >= N ||a[y][x] == 1)
+	if (dir == 0 || dir == 2)
 	{
-		return 0;
-	}
-	if (dir == 2)
-	{
-		if (a[y - 1][x] == 1 || a[y][x - 1] == 1)
+		if (!a[y][x])
 		{
-			return 0;
+			return true;
 		}
-	}
-	if (y == N - 1 && x == N - 1)
-	{
-		return 1;
-	}
-	if (check[dir][y][x])
-	{
-		return check[dir][y][x];
-	}
-	if (dir == 0)
-	{
-		check[dir][y][x] = Func(0, y, x + 1) + Func(2, y + 1, x + 1);
 	}
 	else if (dir == 1)
 	{
-		check[dir][y][x] = Func(1, y + 1, x) + Func(2, y + 1, x + 1);
-	}
-	else
-	{
-		check[dir][y][x] = Func(0, y, x + 1) + Func(1, y + 1, x) + Func(2, y + 1, x + 1);
-	}
-	return check[dir][y][x];
-}
+		if (a[y][x] == 0 && a[y - 1][x] == 0 && a[y][x - 1] == 0)
+		{
+			return true;
+		}
 
+	}
+	return false;
+}
 int main()
 {
-	cin >> N;
-	for (int i = 0; i < N; i++)
+	cin >> n;
+	for (int i = 1; i <= n; i++)
 	{
-		for (int j = 0; j < N; j++)
+		for (int j = 1; j <= n; j++)
 		{
 			cin >> a[i][j];
 		}
 	}
-	cout << Func(0, 0, 1);
+	dp[1][2][0] = 1;
+	for (int i = 1; i <= n; i++)
+	{
+		for (int j = 1; j <= n; j++)
+		{
+			if (check(i, j + 1, 0))
+			{
+				dp[i][j + 1][0] += dp[i][j][0];
+			}
+			if (check(i + 1, j + 1, 1))
+			{
+				dp[i + 1][j + 1][1] += dp[i][j][0];
+			}
 
-	return 0;
+			if (check(i + 1, j, 2))
+			{
+				dp[i + 1][j][2] += dp[i][j][2];
+			}
+			if (check(i + 1, j + 1, 1))
+			{
+				dp[i + 1][j + 1][1] += dp[i][j][2];
+			}
+
+			if (check(i, j + 1, 0))
+			{
+				dp[i][j + 1][0] += dp[i][j][1];
+			}
+			if (check(i + 1, j, 2))
+			{
+				dp[i + 1][j][2] += dp[i][j][1];
+			}
+			if (check(i + 1, j + 1, 1))
+			{
+				dp[i + 1][j + 1][1] += dp[i][j][1];
+			}
+		}
+	}
+	cout << dp[n][n][0] + dp[n][n][1] + dp[n][n][2];
 }
