109cc9c2a4717318803cc5e8f95bdce52ec11083
moad6127 <moad6127@gmail.com>
2024-03-24 19:17:48 +0900

    Feat : 코딩연습
    백준 1513 인터넷 강의 풀이
    dp의 상태값이 y,x,cnt,postC로 4가지로 한후 기저사례좀더 생각 해서 풀어보자

--- CodingTestAlgorithm/Week7/Week7_K_1513/main.cpp
+++ CodingTestAlgorithm/Week7/Week7_K_1513/main.cpp
@@ -2,70 +2,57 @@
 
 using namespace std;
 
-int N, M,C,temp = 1;
-long long m[51][51];
-const int dy[] = {0, 1};
-const int dx[] = { 1,0 };
+int N, M, C;
+int m[51][51];
 int a, b;
-long long l[51][51][51];
-long long Func(int x, int y,int postC,int cCnt)
+int l[51][51][51][51];
+int Func(int y, int x, int postC, int cCnt)
 {
-	//cout << "x : " << x << " y : " << y << '\n';
-	if (m[x][y])
-	{
-		if (cCnt)
-		{
-			if (postC)
-			{
-				if (m[a][b] < postC)
-				{
-					return 0;
-				}
-			}
-			postC = m[x][y];
-			cCnt--;
-		}
-		else
-		{
-			return 0;
-		}
-	}
-	if (x == N && y == M)
-	{
-		if (cCnt == 0)
-		{
-			return 1;
-		}
-		else
-		{
-			return 0;
-		}
-	}
-	if (x<0 || x>N || y<0 || y>M)
+	if (y > N || x > M)
 	{
 		return 0;
 	}
-
-	if (l[cCnt][x][y])
+	if (y == N && x == M)
 	{
-		return l[cCnt][x][y];
+		if (cCnt == 0 && m[y][x] == 0)
+		{
+			return 1;
+		}
+		if (cCnt == 1 && m[y][x] > postC)
+		{
+			return 1;
+		}
+		return 0;
 	}
-
-	l[cCnt][x][y] = Func(x + 1, y, postC,cCnt) + Func(x, y + 1, postC,cCnt);
-
-	return l[cCnt][x][y];
+	if (l[y][x][cCnt][postC] != -1)
+	{
+		return l[y][x][cCnt][postC];
+	}
+	l[y][x][cCnt][postC] = 0;
+	if (m[y][x] == 0)
+	{
+		l[y][x][cCnt][postC] = (Func(y + 1, x, postC, cCnt) + Func(y, x + 1, postC, cCnt)) % 1000007;
+	}
+	else if (m[y][x] > postC)
+	{
+		l[y][x][cCnt][postC] = (Func(y + 1, x, m[y][x], cCnt - 1) + Func(y, x + 1, m[y][x], cCnt - 1)) % 1000007;
+	}
+	return l[y][x][cCnt][postC];
 }
 int main()
 {
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
 	cin >> N >> M >> C;
+	memset(l, -1, sizeof(l));
 	for (int i = 0; i < C; i++)
 	{
 		cin >> a >> b;
-		m[a][b] = temp++;
+		m[a][b] = i + 1;
 	}
 	for (int i = 0; i <= C; i++)
 	{
-		memset(l, 0, sizeof(l));
-		cout<<Func(1, 1, 0, i)<<'\n';
+		cout << Func(1, 1, 0, i) << '\n';
 	}
 }
