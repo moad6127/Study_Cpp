66de29261897b18ef9e4b3a26205b617e81b9d4f
moad6127 <moad6127@gmail.com>
2024-03-27 16:14:30 +0900

    Feat : 코딩연습
    인강 풀이
    재귀방식말고
    오른쪽에서 왼쪽으로 진행하며 풀이했다.

--- CodingTestAlgorithm/Week7/Week7_L_1535/main.cpp
+++ CodingTestAlgorithm/Week7/Week7_L_1535/main.cpp
@@ -1,38 +1,64 @@
+//#include <bits/stdc++.h>
+//
+//using namespace std;
+//
+//int N;
+//int a[21], b[21];
+//int dp[104][21];
+//const int MAX = 987654321;
+//int Func(int nowH,int here)
+//{
+//	if (nowH <= 0)
+//	{
+//		return -MAX;
+//	}
+//	if (here >= N)
+//	{
+//		return 0;
+//	}
+//	if (dp[nowH][here])
+//	{
+//		return dp[nowH][here];
+//	}
+//	dp[nowH][here] = max(Func(nowH, here + 1), Func(nowH - a[here], here + 1) + b[here]);
+//	return dp[nowH][here];
+//}
+//int main()
+//{
+//	cin >> N;
+//	for (int i = 0; i < N; i++)
+//	{
+//		cin >> a[i];
+//	}
+//	for (int i = 0; i < N; i++)
+//	{
+//		cin >> b[i];
+//	}
+//	cout << Func(100, 0);
+//}
+
 #include <bits/stdc++.h>
 
 using namespace std;
 
-int N;
-int a[21], b[21];
-int dp[104][21];
-const int MAX = 987654321;
-int Func(int nowH,int here)
-{
-	if (nowH <= 0)
-	{
-		return -MAX;
-	}
-	if (here >= N)
-	{
-		return 0;
-	}
-	if (dp[nowH][here])
-	{
-		return dp[nowH][here];
-	}
-	dp[nowH][here] = max(Func(nowH, here + 1), Func(nowH - a[here], here + 1) + b[here]);
-	return dp[nowH][here];
-}
+int n, dp[101], cost[101], happy[101];
 int main()
 {
-	cin >> N;
-	for (int i = 0; i < N; i++)
+	cin >> n;
+	for (int i = 0; i < n; i++)
 	{
-		cin >> a[i];
+		cin >> cost[i];
 	}
-	for (int i = 0; i < N; i++)
+	for (int i = 0; i < n; i++)
 	{
-		cin >> b[i];
+		cin >> happy[i];
 	}
-	cout << Func(100, 0);
+	for (int i = 0; i < n; i++)
+	{
+		for (int j = 100; j > cost[i]; j--)
+		{
+			dp[j] = max(dp[j], dp[j - cost[i]] + happy[i]);
+		}
+	}
+	cout << dp[100];
 }
