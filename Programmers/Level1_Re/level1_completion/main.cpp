a2c7b55d7a809e6a64734346fcd77a38f96893fa
moad6127 <moad6127@gmail.com>
2024-05-12 13:42:40 +0900

    Feat : 코딩연습
    코딩연습

--- Programmers/Level1_Re/level1_completion/main.cpp
+++ Programmers/Level1_Re/level1_completion/main.cpp
@@ -7,7 +7,7 @@ string solution(vector<string> participant, vector<string> completion) {
 
     sort(participant.begin(), participant.end());
     sort(completion.begin(), completion.end());
-    string answer = completion.back();
+    string answer = participant.back();
     for (int i = 0; i < completion.size(); i++)
     {
         if (participant[i] != completion[i])
