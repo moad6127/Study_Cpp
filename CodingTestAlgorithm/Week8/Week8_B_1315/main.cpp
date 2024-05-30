4bdfede86f97e90ca32ba354e567550df66e81f2
moad6127 <moad6127@gmail.com>
2024-05-30 16:55:28 +0900

    Feat : 코딩연습
    백준 1315 인강 풀이
    최대값을 잘설정한다 ,
    방문처리를 잘 처리해야 한다

--- CodingTestAlgorithm/Week8/Week8_B_1315/main.cpp
+++ CodingTestAlgorithm/Week8/Week8_B_1315/main.cpp
@@ -7,43 +7,45 @@ struct st
 	int in;
 	int pt;
 };
+
 int N,str,in,pt;
 vector<st> v;
 int dp[1004][1004];
-int Func(int nowSTR, int nowINT, int nowPT, vector<int> clearQ, int count)
+bool visited[101];
+int Func(int nowSTR, int nowINT)
 {
-	int cnt{};
-
-	for (int i = 0 ; i<v.size();i++)
-	{
-		if (clearQ[i])
-		{
-			cnt++;
-			continue;
-		}
-		if (nowSTR >= v[i].st || nowINT >= v[i].in)
-		{
-			nowPT += v[i].pt;
-			clearQ[i] = 1;
-			cnt++;
-		}
-	}
-	if (cnt < count)
-	{
-		return 0;
-	}
-	if (cnt == count || cnt == clearQ.size())
-	{
-		return cnt;
-	}
-	if (dp[nowSTR][nowINT])
+	if (dp[nowSTR][nowINT] != -1)
 	{
 		return dp[nowSTR][nowINT];
 	}
-	for (int i = 0; i <= nowPT; i++)
+	dp[nowSTR][nowINT] = 0;
+	int ptr{};
+	vector<int> visit;
+	for (int i = 0 ; i<N;i++)
 	{
-		dp[nowSTR][nowINT] = max(dp[nowSTR][nowINT], Func(nowSTR + i, nowINT + nowPT - i, 0, clearQ,cnt));
+		if (nowSTR >= v[i].st || nowINT >= v[i].in)
+		{
+			dp[nowSTR][nowINT]++;
+			if (!visited[i])
+			{
+				ptr += v[i].pt;
+				visited[i] = true;
+				visit.push_back(i);
+			}
+		}
 	}
+	for (int i = 0; i <= ptr; i++)
+	{
+		int nextSTR = min(1000, nowSTR + i);
+		int nextINT = min(1000, nowINT + ptr - i);
+		dp[nowSTR][nowINT] = max(dp[nowSTR][nowINT], Func(nextSTR, nextINT));
+	}
+
+	for (auto e : visit)
+	{
+		visited[e] = false;
+	}
+
 	return dp[nowSTR][nowINT];
 }
 int main()
@@ -52,12 +54,13 @@ int main()
 	cin.tie(NULL);
 	cout.tie(NULL);
 	cin >> N;
-	vector<int> clear(N, 0);
+	vector<int> clear(N + 1, 0);
+	memset(dp, -1, sizeof(dp));
 	for (int i = 0;i < N; i++)
 	{
 		st s;
 		cin >> s.st >> s.in >> s.pt;
 		v.push_back(s);
 	}
-	cout<<Func(1, 1, 0, clear, 0);
+	cout<<Func(1, 1);
 }
