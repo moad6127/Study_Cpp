6ef82ffec08421b12a295f67783f4971a87f934b
moad6127 <moad6127@gmail.com>
2024-06-08 15:02:39 +0900

    Feat: 코딩연습
    백준 5419 펜윅트리, 좌표압축, 정렬등을 사용해서 풀이하기

--- CodingTestAlgorithm/Week8/Week8_D_5419/main.cpp
+++ CodingTestAlgorithm/Week8/Week8_D_5419/main.cpp
@@ -3,35 +3,79 @@
 using namespace std;
 
 int T, N, x,y;
+vector<int> tree, _y;
+vector<pair<int, int>> a;
 
+int Sum(int idx)
+{
+	int result{};
+	while (idx > 0)
+	{
+		result += tree[idx];
+		idx -= (idx & -idx);
+	}
+	return result;
+}
+
+void Update(int pos, int v)
+{
+	int idx = pos;
+	while (idx <= N)
+	{
+		tree[idx] += v;
+		idx += (idx & -idx);
+	}
+}
+int FindIdx(const vector<int>& _y, int v)
+{
+	int lo{};
+	int hi = _y.size() - 1;
+	while (lo <= hi)
+	{
+		int mid = (lo + hi) / 2;
+		if (_y[mid] == v)
+		{
+			return mid;
+		}
+		if (_y[mid] < v)
+		{
+			lo = mid + 1;
+		}
+		else
+		{
+			hi = mid - 1;
+		}
+	}
+}
 int main()
 {
 	cin >> T;
 	for (int t = 0; t < T; t++)
 	{
-		vector<pair<int, int>> v;
-		int cnt{};
 		cin >> N;
+		tree.clear();
+		tree.resize(N + 1);
+		a.clear();
+		_y.clear();
 		for (int i = 0; i < N; i++)
 		{
 			cin >> x >> y;
-			v.push_back({ x,y });
+			a.push_back({ x, y * -1 });
+			_y.push_back(y * -1);
 		}
-		for (int i = 0; i < v.size(); i++)
+		sort(a.begin(), a.end());
+		sort(_y.begin(), _y.end());
+		long long result = 0;
+
+		Update(FindIdx(_y, a[0].second) + 1, 1);
+		for (int i = 1; i < N; i++)
 		{
-			for (int j = 0; j < v.size(); j++)
-			{
-				if (i == j)
-				{
-					continue;
-				}
-				if (v[i].first >= v[j].first && v[i].second <= v[j].second)
-				{
-					cnt++;
-				}
-			}
+			int idx = FindIdx(_y, a[i].second) + 1;
+			result += (long long)1 * Sum(idx);
+			Update(idx, 1);
 		}
-		cout << cnt<<'\n';
+
+		cout << result<<'\n';
 	}
 
 }
