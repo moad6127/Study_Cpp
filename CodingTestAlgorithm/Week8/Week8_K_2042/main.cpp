b2b8589e7832d9b515afb26680338b4d5f9b6dda
moad6127 <moad6127@gmail.com>
2024-07-07 18:35:39 +0900

    Feat : 코딩연습
    백준 2042 펜윅트리를 사용한 구간합 문제
    vector대신 배열 사용

--- CodingTestAlgorithm/Week8/Week8_K_2042/main.cpp
+++ CodingTestAlgorithm/Week8/Week8_K_2042/main.cpp
@@ -3,9 +3,11 @@
 using namespace std;
 
 int N, M, K;
-int A, B, C;
+long long tree[1000004];
+long long a[1000004];
+long long A, B, C;
 
-long long Sum(vector<long long>& tree, int i)
+long long Sum(int i)
 {
 	long long result{};
 	while (i > 0)
@@ -15,9 +17,9 @@ long long Sum(vector<long long>& tree, int i)
 	}
 	return result;
 }
-void Update(vector<long long>& tree, int idx, long long val)
+void Update(int idx, long long val)
 {
-	while (idx <tree.size())
+	while (idx <= 1000003)
 	{
 		tree[idx] += val;
 		idx += (idx & -idx);
@@ -26,26 +28,28 @@ void Update(vector<long long>& tree, int idx, long long val)
 
 int main()
 {
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+
 	cin >> N >> M >> K;
-	vector<long long> a(N + 1);
-	vector<long long> tree(N + 1);
 	for (int i = 1; i <= N; i++)
 	{
 		cin >> a[i];
-		Update(tree,i, a[i]);
+		Update(i, a[i]);
 	}
-	for (int i = 0; i < M + K; i++)
+	for (int test = 0; test < M + K; test++)
 	{
 		cin >> A >> B >> C;
 		if (A == 1)
 		{
 			long long temp = C - a[B];
 			a[B] = C;
-			Update(tree,B, temp);
+			Update(B, temp);
 		}
 		else
 		{
-			cout << Sum(tree,C) - Sum(tree,B - 1) << '\n';
+			cout << Sum(C) - Sum(B - 1)<<'\n';
 		}
 	}
 }
