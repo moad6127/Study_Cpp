dfdfc351ad99c6c1fb1d336bc9796a53ef0f5cb9
moad6127 <moad6127@gmail.com>
2024-07-10 18:51:02 +0900

    Feat : 코딩연습
    백준 11658
    펜윅트리를 2중배열로 만들어서 사용하기

---
+++ CodingTestAlgorithm/Week8/Week8_l_11658/main.cpp
@@ -0,0 +1,65 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int n, m;
+int w;
+int a[1028][1028];
+int tree[1028][1028];
+void Update(int y, int x, int value)
+{
+	while (y <= n)
+	{
+		int j = x;
+		while (j <= n)
+		{
+			tree[y][j] += value;
+			j += (j & -j);
+		}
+		y += (y & -y);
+	}
+}
+int Sum(int y, int x)
+{
+	int s = 0;
+	while (y > 0)
+	{
+		int j = x;
+		while (j > 0)
+		{
+			s += tree[y][j];
+			j -= (j & -j);
+		}
+		y -= (y & -y);
+	}
+	return s;
+}
+int main()
+{
+	cin >> n >> m;
+	for (int i = 1; i <= n; i++)
+	{
+		for (int j = 1; j <= n; j++)
+		{
+			cin >> a[i][j];
+			Update(i, j, a[i][j]);
+		}
+	}
+	for (int i = 0; i < m; i++)
+	{
+		cin >> w;
+		if (w)
+		{
+			int y1{}, x1{}, y2{}, x2{};
+			cin >> y1 >> x1 >> y2 >> x2;
+			cout << Sum(y2, x2) - Sum(y1 - 1, x2) - Sum(y2, x1 - 1) + Sum(y1 - 1, x1 - 1) << '\n';
+		}
+		else
+		{
+			int y{}, x{}, c{};
+			cin >> y >> x >> c;
+			Update(y, x, c - a[y][x]);
+			a[y][x] = c;
+		}
+	}
+}
