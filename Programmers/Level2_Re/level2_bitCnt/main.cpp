7dd8089e077f65f1cec79f609aa63ded3215199b
moad6127 <moad6127@gmail.com>
2024-07-27 19:36:40 +0900

    Feat ; 코딩연습
    프로그래머스 level2

--- Programmers/Level2_Re/level2_bitCnt/main.cpp
+++ Programmers/Level2_Re/level2_bitCnt/main.cpp
@@ -9,24 +9,6 @@ vector<long long> solution(vector<long long> numbers) {
     vector<long long> answer;
     for (auto e : numbers)
     {
-        long long next = e;
-        while (true)
-        {
-            next++;
-            int cnt{};
-            for (long long i = 0; i < 32; i++)
-            {
-                if ((1&(e >> i)) !=(1&(next >> i)))
-                {
-                    cnt++;
-                }
-            }
-            if (cnt <= 2)
-            {
-                answer.push_back(next);
-                break;
-            }
-        }
     }
     return answer;
 }
