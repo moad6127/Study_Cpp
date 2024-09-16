d9d512a2c55c2f68dbb7bb7575dd6cbbed4e9b06
moad6127 <moad6127@gmail.com>
2024-09-16 14:35:28 +0900

    Feat : 코딩연습
    코딩연습

---
+++ Programmers/Level2_Re/level2_nSqr/main.cpp
@@ -0,0 +1,11 @@
+using namespace std;
+
+long long solution(int w, int h) {
+    long long answer = 0;
+    for (int i = 1; i < w; i++)
+    {
+        long long n = ((double)h * i) / w;
+        answer += n;
+    }
+    return answer * 2;
+}
