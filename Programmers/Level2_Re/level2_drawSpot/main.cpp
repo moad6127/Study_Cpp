0b7eb252542cc10b2e04c99c8196d0a976a62a43
moad6127 <moad6127@gmail.com>
2024-10-13 20:17:43 +0900

    Feat : 코딩연습

--- Programmers/Level2_Re/level2_drawSpot/main.cpp
+++ Programmers/Level2_Re/level2_drawSpot/main.cpp
@@ -8,10 +8,10 @@ long long solution(int k, int d) {
     long long answer = 0;
     // y^2 + x^2 = d^2;
 
-    for (long long i = 0; i <= d; i++)
+    for (long long i = 0; i <= d; i += k)
     {
-        long long y = sqrt((long long(d) * d) - (i * i));
-        answer += floor(y / k) + 1;
+        long long y = sqrt(((long long)d  * d) - (i * i));
+        answer += (long long)floor(y / k) + 1;
     }
     return answer;
 }
